cf833a7737dd5076bcb014fba99d42c77cf70ea0
gabloml <gabo.lomeli@gmail.com>
2026-08-27 16:58:04 -0600

    Update block_ads.sh

--- block_ads.sh
+++ block_ads.sh
@@ -3,7 +3,7 @@
 # Replace these variables with your actual Cloudflare API token and account ID
 API_TOKEN="$API_TOKEN"
 ACCOUNT_ID="$ACCOUNT_ID"
-PREFIX="Block ads"
+PREFIX="Block - OISD Ads"
 MAX_LIST_SIZE=1000
 MAX_LISTS=100
 MAX_RETRIES=10
